472eb74b6e46b220a0f44c843e2bc41977390547
mateus2k2 <mateus2k2@users.noreply.github.com>
2024-06-18 23:33:35 +0000

    deploy: 7281a4e607fe4b7e02e2e5bd194fc70cbcb78ce7

---
+++ en/assets/javascripts/extra.js
@@ -0,0 +1,26 @@
+// document$.subscribe(function() {
+//     var tables = document.querySelectorAll("article table:not([class])")
+//     tables.forEach(function(table) {
+//         new Tablesort(table)
+//     })
+// })
+
+window.MathJax = {
+tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+},
+options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+}
+};
+
+document$.subscribe(() => { 
+MathJax.startup.output.clearCache()
+MathJax.typesetClear()
+MathJax.texReset()
+MathJax.typesetPromise()
+})
